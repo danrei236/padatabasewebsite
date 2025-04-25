e5c8c0546d04ca2501f7b1f0fe6329f7cb4c34e0
danreisaks <danrei236@gmail.com>
2025-04-25 21:02:48 +0200

    update data url

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-#from app import create_app
-
-#app = create_app()
-
-#if __name__ == '__main__':
-#    app.run()
-
 from app import create_app
-import os  # <-- Add this
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use Heroku's PORT or default to 5000 locally
-    app.run(host='0.0.0.0', port=port)  # <-- Bind to 0.0.0.0 and dynamic port
+    app.run()
--- config.py
+++ config.py
@@ -3,5 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///political_party.db'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://u93rujlo0fek1b:pc0748f040f84ae539830f559bb6dd10316316f817ca627b2cf5bb21898288d00@c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d57ucann7r8e8q'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
