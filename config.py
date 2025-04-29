fd99cc97fa97b7ea2fa11675aef033b4947d2804
danreisaks <danrei236@gmail.com>
2025-04-30 01:58:15 +0200

    database url

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///political_party.db'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://u6stgvkaog777g:pd2c6d566e18ed1c6ce7478753e1e1cd12511a9bb381c3f6c0334722473ac348c@c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfgmmtcun2thfr'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///political_party.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://u6stgvkaog777g:pd2c6d566e18ed1c6ce7478753e1e1cd12511a9bb381c3f6c0334722473ac348c@c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfgmmtcun2thfr'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
