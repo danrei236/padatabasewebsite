0578484e60104be5a5863cdf3267f47f28e79ea9
danreisaks <danrei236@gmail.com>
2025-04-30 17:45:15 +0200

    database url update

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///political_party.db'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://u6stgvkaog777g:pd2c6d566e18ed1c6ce7478753e1e1cd12511a9bb381c3f6c0334722473ac348c@c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfgmmtcun2thfr'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://u6stgvkaog777g:pd2c6d566e18ed1c6ce7478753e1e1cd12511a9bb381c3f6c0334722473ac348c@c952v5ogavqpah.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfgmmtcun2thfr'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///political_party.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://uuvjjka53mnm:p719f9bf82e75f3d4a06bb715bdea05f80308926d56487255b5dabb2ef74a280f@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6nl21rb2ui94b'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
